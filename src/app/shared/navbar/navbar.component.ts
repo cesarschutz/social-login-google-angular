074ea08d07aae90d43716932a1759b462c7c9ef0
Cesar Schutz <cesarschutz340@gmail.com>
2021-08-29 14:41:25 -0300

    ajustes img

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -36,12 +36,14 @@ export class NavbarComponent implements OnInit {
 
         this.sidebarVisible = true;
     };
-    sidebarClose() {
+
+    public sidebarClose() {
         const body = document.getElementsByTagName('body')[0];
         this.toggleButton.classList.remove('toggled');
         this.sidebarVisible = false;
         body.classList.remove('nav-open');
     };
+
     sidebarToggle() {
         // const toggleButton = this.toggleButton;
         // const body = document.getElementsByTagName('body')[0];
