22fddcfa7e5aa01cde558d6944f42cd1d27160e0
Cesar Schutz <cesarschutz340@gmail.com>
2021-09-25 12:03:48 -0300

    create patient-detail

---
+++ src/app/patient-detail/patient-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-datil',
+  templateUrl: './patient-detail.component.html',
+  styleUrls: ['./patient-detail.component.css']
+})
+export class PatientDetail implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+declare interface TableData {
+  headerRow: string[];
+  dataRows: string[][];
+}
+
 @Component({
   selector: 'app-patient',
   templateUrl: './patient.component.html',
@@ -7,9 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientComponent implements OnInit {
 
+  public tableData1: TableData;
+
   constructor() { }
 
   ngOnInit() {
+    this.tableData1 = {
+      headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
+      dataRows: [
+          ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
+          ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
+          ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
+          ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
+          ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
+          ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
+      ]
+  };
   }
 
 }
