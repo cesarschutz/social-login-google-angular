25e04755d32c50f8de401e4c805443176f211f43
Cesar Schutz <cesarschutz340@gmail.com>
2021-09-25 12:15:15 -0300

    patients list

--- src/app/patients/patient/patient.component.ts
+++ src/app/patients/patient/patient.component.ts
@@ -18,14 +18,10 @@ export class PatientComponent implements OnInit {
 
   ngOnInit() {
     this.tableData1 = {
-      headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
+      headerRow: [ 'ID', 'Registry', 'Name', 'Room', 'Responsible'],
       dataRows: [
-          ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
-          ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
-          ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
-          ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
-          ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
-          ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
+          ['1', '12345', 'Michel', '001', 'Michel'],
+          ['2', '12345', 'Cesar', '001', 'Cesar']
       ]
   };
   }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -12,7 +12,7 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
   { path: '/dashboard', title: 'Home', icon: 'pe-7s-graph', class: '' },
   //{ path: '/user', title: 'Profile',  icon:'pe-7s-user', class: '' },
-  { path: '/patient', title: 'Patients',  icon:'pe-7s-id', class: '' },
+  { path: '/patient', title: 'Pacientes',  icon:'pe-7s-id', class: '' },
   //{ path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
   { path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
   //{ path: '/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
