56be00b04e8cfc6e4074abf0277cde256bf7e1de
Cesar Schutz <cesarschutz340@gmail.com>
2021-09-26 16:50:07 -0300

    rafctory

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -4,18 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LbdModule } from '../../lbd/lbd.module';
-import { NguiMapModule} from '@ngui/map';
 
 import { AdminLayoutRoutes } from './admin-layout.routing';
 
 import { HomeComponent } from '../../home/home.component';
 import { UserComponent } from '../../user/user.component';
-import { TablesComponent } from '../../tables/tables.component';
 import { PatientComponent } from '../../patients/patient/patient.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import { PatientDetail } from 'app/patients/patient-detail/patient-detail.component';
 import { VitalSignsComponent } from 'app/patients/vital-signs-list/vital-signs.component';
 import { VitalSignsCreateComponent } from 'app/patients/vital-signs-create/vital-signs-create.component';
@@ -27,20 +21,14 @@ import { VitalSignsCreateComponent } from 'app/patients/vital-signs-create/vital
     RouterModule.forChild(AdminLayoutRoutes),
     FormsModule,
     LbdModule,
-    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=YOUR_KEY_HERE'})
   ],
   declarations: [
     HomeComponent,
     UserComponent,
-    TablesComponent,
     PatientComponent,
     PatientDetail,
     VitalSignsComponent,
-    VitalSignsCreateComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent
+    VitalSignsCreateComponent
   ]
 })
 
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -12,16 +12,11 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
   { viewInMenu: true, path: '/dashboard', title: 'Home', icon: 'pe-7s-graph', class: '' },
-  //{ path: '/user', title: 'Profile',  icon:'pe-7s-user', class: '' },
   { viewInMenu: true, path: '/patients', title: 'Pacientes',  icon:'pe-7s-users', class: '' },
   { viewInMenu: false, path: '/patient-detail', title: 'Paciente',  icon:'pe-7s-id', class: '' },
   { viewInMenu: false, path: '/vital-signs', title: 'Sinais Vitais',  icon:'pe-7s-note2', class: '' },
   { viewInMenu: false, path: '/vital-signs-create', title: 'Cadastrar Sinais Vitais',  icon:'pe-7s-note2', class: '' },
-  //{ path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
   { viewInMenu: true, path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
-  //{ path: '/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
-  //{ path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
-  //{ path: '/upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
 ];
 
 @Component({
